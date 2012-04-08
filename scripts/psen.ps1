f4b554d279686ed748658c5c35dcfdafb0fd681b
Ole Lynge Soerensen <olelynge@gmail.com>
2012-04-08 10:00:47 +0200

    Remove obsolete scripts.

--- scripts/psen.ps1
+++
@@ -1,9 +0,0 @@
-$task = $args[0]
-
-$psakeVersion = "4.0.1.0"
-
-nuget install psake -version $psakeVersion -o packages
-
-Import-Module .\packages\psake.$psakeVersion\tools\psake.psm1
-
-& ".\packages\psake.$psakeVersion\tools\psake.cmd" .\scripts\default.ps1 $task
