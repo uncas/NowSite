b4f9fb3801bd42962640ebb9d16d08983453ba00
Ole Lynge Soerensen <olelynge@gmail.com>
2012-07-08 09:36:01 +0200

    Add logon page.

---
+++ src/Uncas.NowSite.Web/Controllers/AuthenticationController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using Uncas.NowSite.Web.Models;
+
+namespace Uncas.NowSite.Web.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        [HttpGet]
+        public ActionResult LogOn()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogOn(LogOnInputModel model)
+        {
+            if (model.UserName == "test" &&
+                model.Password == "test123")
+            {
+                FormsAuthentication.SetAuthCookie(model.UserName, true);
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
