1932ae754a8cf3968375e7bfede45556b743adaf
Ole Lynge Soerensen <olelynge@gmail.com>
2012-07-08 09:53:34 +0200

    Add page for creating blog post.

---
+++ src/Uncas.NowSite.Web/Controllers/BlogPostController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Mvc;
+using Uncas.NowSite.Web.Models;
+
+namespace Uncas.NowSite.Web.Controllers
+{
+    public class BlogPostController : Controller
+    {
+        [HttpGet]
+        public ActionResult Create()
+        {
+            Guid id = Guid.NewGuid();
+            return View(new CreateBlogPostInputModel { Id = id });
+        }
+
+        [HttpPost]
+        public ActionResult Create(CreateBlogPostInputModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ src/Uncas.NowSite.Web/Models/CreateBlogPostInputModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Uncas.NowSite.Web.Models
+{
+    public class CreateBlogPostInputModel
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+}
