c325c3f5a12adf7aff5e7ac3a643260dc25cfd30
Ole Lynge Soerensen <olelynge@gmail.com>
2012-07-26 14:31:48 +0200

    Change ftp folder.

--- scripts/custom.ps1
+++ scripts/custom.ps1
@@ -3,11 +3,12 @@ function LoadCustomProperties {
     $script:FtpHost = "ftp.example.com"
     $script:FtpUser = "FtpUser"
     $script:FtpPassword = "FtpPassword"
+    $script:FtpFolder = "new"
 }
 
 function CustomPublish {
     "Custom publish in progress!"
     $localFolder = "$collectDir\Uncas.NowSite.Web"
-    SynchronizeFoldersViaFtp $script:FtpHost $script:FtpUser $script:FtpPassword $localFolder "new"
+    SynchronizeFoldersViaFtp $script:FtpHost $script:FtpUser $script:FtpPassword $localFolder $script:FtpFolder
     "Custom publish done!"
 }
